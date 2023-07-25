e644f3c25ad34ec5fc7667464c23143aa6f46e59
EsheBT <eshetu9244@yahoo.com>
2023-07-25 11:33:52 -0700

    t3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
-"""Exports to-do list information of all employees to JSON format."""
+"""Export to csv the information about user TODO list progress"""
 import json
 import requests
 
+
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
+    uri_user = "users"
+    uri_todos = "todos"
+
+    users = requests.get(url + uri_user).json()
+    dir_user_tasks = {}
+
+    for user in users:
+        user_id = user.get("id")
+        user_name = user.get("username")
+        tasks = requests.get(url + uri_todos,
+                             params={"userId": user_id}).json()
+        user_tasks = ({user_id: [{"task": task.get("title"),
+                                  "completed": task.get("completed"),
+                                  "username": user_name} for task in tasks]})
+        dir_user_tasks.update(user_tasks)
 
     with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+        json.dump(dir_user_tasks, jsonfile)
